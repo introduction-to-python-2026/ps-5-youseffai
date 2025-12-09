d88d8cc712f3781ee44e1e7b936dbb6102291e6a
youseffai <altyoxox@gmail.com>
2025-12-09 20:24:24 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -39,10 +39,26 @@ def split_before_each_uppercases(text):
     
   return segments
 def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.  
+    """Takes a molecular formula (string) and returns a dictionary of atom counts.
     Example: 'H2O' → {'H': 2, 'O': 1}"""
 
     # Step 1: Initialize an empty dictionary to store atom counts
+    dct={}
+    list=split_before_each_uppercases(molecular_formula)
+    for item in list:
+      for i in item:
+        if i.isdigit():
+          index=item.index(i)
+          num=int(item[index:])
+          name=item[:index]
+        else:
+          name=item
+          num=1
+        
+        # Step 2: Update the dictionary with the atom name and count
+      dct[name]=num
+    # Step 3: Return the completed dictionary
+    return dct
 
     for atom in split_by_capitals(molecular_formula):
         atom_name, atom_count = split_at_number(atom)
